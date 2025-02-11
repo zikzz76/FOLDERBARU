baeb94ac19349f33c2dd32bbe3dfcb809a3168e8
sitiaminah09 <aminahsiti5157@gmail.com>
2025-02-11 07:43:53 +0700

    menambahkan tugas

---
+++ latihan 1/latihan1.py
@@ -0,0 +1,10 @@
+def cetakIdentitasSiswa(nama,kelas,jenis_kelamin):
+    print("Nama : ",nama)
+    print("Kelas : ",kelas)
+    print("Jenis kelamin : ",jenis_kelamin)
+    
+# panggil fungsi cetak identitas siswa
+cetakIdentitasSiswa("zikzz","x-ppl","laki-laki")
+cetakIdentitasSiswa("hamzz","x-ppl","laki-laki")
+cetakIdentitasSiswa("apenzz","x-ppl","laki-laki")
+cetakIdentitasSiswa("waizz","x-ppl","laki-laki")
